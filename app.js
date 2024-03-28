cd21d85f0f02229debe099fb3a23b2bdd3d912b4
Santan21 <lesliegyamfi02@gmail.com>
2024-03-28 13:09:46 +0000

    chore: fixed server error and modified json data

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-const fs = require('fs');
-const express  = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
 const blogs = JSON.parse(
-    fs.readFileSync(`${__dirname}/assets/blog-data/blogs-simple.json`)
-  );
+  fs.readFileSync(`${__dirname}/assets/blog-data/blogs-simple.json`)
+);
 
 app.get("/api/v1/blogs", (req, res) => {
-    res.status(200).json({
-        status: 'Success',
-        results: blogs.length,
-        data: {
-            blogs,
-        }
-    })
+  res.status(200).json({
+    status: "Success",
+    results: blogs.length,
+    data: {
+      blogs,
+    },
+  });
 });
 
 app.post("/api/v1/blogs", (req, res) => {
-    const newId = blogs[blogs.length - 1].id + 1;
-    const newBlog = Object.assign({ id: newId }, req.body);
-  
-    blogs.push(newBlog);
-    fs.writeFile(
-      `${__dirname}/assets/data/blogs-simple.json`,
-      JSON.stringify(blogs),
-      (err) => {
-        res.status(201).json({
-          status: "success",
-          data: {
-            blog: newBlog,
-          },
-        });
-      }
-    );
-  });
+  const newId = blogs[blogs.length - 1].id + 1;
+  const newBlog = Object.assign({ id: newId }, req.body);
+
+  blogs.push(newBlog);
+  fs.writeFile(
+    `${__dirname}/assets/data/blogs-simple.json`,
+    JSON.stringify(blogs),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          blog: newBlog,
+        },
+      });
+    }
+  );
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log("ST-BLOG-API is running on port 8000");
+});
