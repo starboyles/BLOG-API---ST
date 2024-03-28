49d5560f04455425f262805c6b3e4a9931c3bb63
Santan21 <lesliegyamfi02@gmail.com>
2024-03-28 14:10:49 +0000

    Handled PATCH Requests

--- app.js
+++ app.js
@@ -38,9 +38,14 @@ app.post("/api/v1/blogs", (req, res) => {
   );
 });
 
-app.patch("/api/v1/blogs", (req, res)=> {
-    res.status(200).json
-})
+app.patch("/api/v1/blogs", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      blog: "Updated Blog>",
+    },
+  });
+});
 
 const port = 8000;
 app.listen(port, () => {
