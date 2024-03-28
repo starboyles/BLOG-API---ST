a801eea0fe9448c5b315458b33ab598f2be27e0a
Santan21 <lesliegyamfi02@gmail.com>
2024-03-28 12:41:00 +0000

    Handled GET Requests

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
 const express  = require('express');
 const app = express();
 
+const blogs = JSON.parse(
+    fs.readFileSync(`${__dirname}/assets/blog-data/blogs-simple.json`)
+  );
+
+
+
 app.get("/api/v1/blogs", (req, res) => {
-    res.status.json(200);
-})
+    res.status(200).json({
+        status: 'Success',
+        results: blogs.length,
+        data: Blogs
+    })
+});
+
+app.post("/api/v1/blogs", (req, res) => {
+    res.status(200).json({
+
+        
+    })
+;
+});
