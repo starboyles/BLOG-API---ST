5b2d4ecd72bcb01b30925aef1cbc43ebfa68ada0
Santan21 <lesliegyamfi02@gmail.com>
2024-04-04 10:51:06 +0000

    creating blogSchema and formatting JSON data

--- server.js
+++ server.js
@@ -15,20 +15,6 @@ mongoose
 })
   .then(() =>
     console.log('DB Connection Successful'));
-    
-    const blogSchema = new mongoose.Schema({
-      id: {
-        type: 'number',
-        required: [true, 'A blog must have an id'],
-        unique: true,
-      },
-      title: { 
-        type: 'string',
-        required: [true, 'A blog must have a title']
-      }
-    });
-    const Blog = mongoose.model('Blog', blogSchema);
-
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
