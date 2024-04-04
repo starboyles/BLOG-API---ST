14aee99f50207834c5f0211ae13f9cab27b56bc9
Santan21 <lesliegyamfi02@gmail.com>
2024-04-04 10:00:19 +0000

    modelling users

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+//userSchema
+//name, email, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Make your name  known']
+    },
+    email: {
+        type: String,
+        required: [true, 'Provide your email address'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Provide a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Provide your password'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Confirm your password'],
+        
+    }
+
+
+
+});
