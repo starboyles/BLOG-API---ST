a8cc20ecacb43f01abe1ad6669ceae573e9ebd6d
Santan21 <lesliegyamfi02@gmail.com>
2024-04-04 12:28:54 +0000

    modifying blog Model

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,27 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    id: {
-      type: 'number',
-      required: [true, 'A blog must have an id'],
-      unique: true,
-    },
-    title: { 
-      type: 'string',
-      required: [true, 'A blog must have a title']
-    },
-    duration: {
-        type: integer,
-        required: [true, 'A blog must have a duration'],
-    },
-    difficulty: {
-    type: integer,
-    required: [true, 'A blog must have a difficulty'],
-    },
+  id: {
+    type: "number",
+    required: [true, "A blog must have an id"],
+    unique: true,
+  },
+  title: {
+    type: "string",
+    required: [true, "A blog must have a title"],
+  },
+  duration: {
+    type: number,
+    required: [true, "A blog must have a duration"],
+  },
+  difficulty: {
+    type: number,
+    required: [true, "A blog must have a difficulty"],
+    validate: [validator.isDifficulty, 'Provide blog difficulty level']
+  },
 
-    description:{
+  description: {
+    type: string,
+    required: [true, "A blog must have a description"],
+  },
 
-    }
-  });
+});
 
-  const Blog = mongoose.model('Blog', blogSchema);
+const Blog = mongoose.model("Blog", blogSchema);
