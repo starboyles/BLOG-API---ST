c435ce4e283197030a7f3495bee97f18af7eb507
Santan21 <lesliegyamfi02@gmail.com>
2024-04-04 12:19:29 +0000

    creating blogSchema

--- models/blogModel.js
+++ models/blogModel.js
@@ -9,6 +9,19 @@ const blogSchema = new mongoose.Schema({
     title: { 
       type: 'string',
       required: [true, 'A blog must have a title']
+    },
+    duration: {
+        type: integer,
+        required: [true, 'A blog must have a duration'],
+    },
+    difficulty: {
+    type: integer,
+    required: [true, 'A blog must have a difficulty'],
+    },
+
+    description:{
+
     }
   });
+
   const Blog = mongoose.model('Blog', blogSchema);
