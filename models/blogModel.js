718ce836ff0858b7dbbe4ad6e91c3e3b0a732f22
Santan21 <lesliegyamfi02@gmail.com>
2024-04-05 19:54:28 +0000

    done fixing blogModel schema typos

--- models/blogModel.js
+++ models/blogModel.js
@@ -2,26 +2,26 @@ const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
   id: {
-    type: number,
+    type: Number,
     required: [true, 'A blog must have an id'],
     unique: true,
   },
   title: {
-    type: string,
+    type: String,
     required: [true, 'A blog must have a title'],
     trim: true,
   },
   duration: {
-    type: number,
+    type: Number,
     required: [true, 'A blog must have a duration'],
   },
   difficulty: {
-    type: number,
+    type: Number,
     required: [true, 'A blog must have a difficulty'],
     validate: [validator.isDifficulty, 'Provide blog difficulty level'],
   },
   description: {
-    type: string,
+    type: String,
     required: [true, 'A blog must have a description'],
     trim: true,
   },
