faece440a9f36c3b542c96d97bcc87b76b64cea7
Santan21 <lesliegyamfi02@gmail.com>
2024-04-05 13:53:27 +0000

    chore: finished importing data into database

--- assets/blog-data/import-blog-data.js
+++ assets/blog-data/import-blog-data.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const Blog = require('../../models/blogModel');
 
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
@@ -16,18 +17,20 @@ mongoose
   .then(() => console.log('DB Connection Successful'));
 
 //READ JSON FILE
-
 const blogs = JSON.parse(
     fs.readFileSync(`${__dirname}/blogs-simple.json`, 'utf-8')
 );
 
 //IMPORTING DATA INTO DATABASE
-
 const importData = async () => {
     try{
-
+        await Blog.create(blogs);
+        console.log('Data Successfully Loaded');
     }
     catch(err) {
         console.log(err);
     }
-}
+};
+
+// DELETING DATA FROM DATABASE
+
