77c38a9f2fbc5aca93942f8468aba9964785e7c5
Santan21 <lesliegyamfi02@gmail.com>
2024-04-07 23:53:42 +0000

    implemented deleteBlog function successfully

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -107,9 +107,23 @@ exports.updateBlog = async (req, res) => {
   }
 };
 
-exports.deleteBlog = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndDelete(req.params.id);
+    if (!blog) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Blog not found',
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
