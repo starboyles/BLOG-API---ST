2d5a7593d78302a31941c86fb751c5f089383b6c
Santan21 <lesliegyamfi02@gmail.com>
2024-04-08 14:04:30 +0000

    fixed asyn typo

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.signup = catchAync(async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
   res.status(201).json({
