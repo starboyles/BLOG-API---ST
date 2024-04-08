752548cd96310f99e4f88894ac18ddefdf35a35a
Santan21 <lesliegyamfi02@gmail.com>
2024-04-08 13:47:00 +0000

    creating new users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
 const userRouter = express.Router();
+
+
+Router.post('/signup', )
 
 userRouter
   .route('/')
