4c0eb9d8463d62ee373a1ea51111c1cda9ef1baa
Santan21 <lesliegyamfi02@gmail.com>
2024-04-11 16:47:52 +0000

    loggin in users

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,19 +24,33 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const { email, password } = req.body;
   
-  //check if email and password do exist
-  if(!email || !password) {
-    next(new AppError('Provide email and password', 400));
+  // check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Provide email and password', 400));
+  }
 
-  //check if user exits & password are correct
+  try {
+    // check if user exists & password is correct
+    const user = await User.findOne({ email }).select('+password');
+    
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('Incorrect email or password', 401));
+    }
 
-  //send token to client if everything is on point
-    const token = '';
-    res.status(201).json({
-      status: 'success',
-        token 
+    // send token to client if everything is on point
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN 
     });
-}};
+
+    res.status(200).json({
+      status: 'success',
+      token 
+    });
+  } catch (error) {
+    return next(new AppError('Internal server error', 500));
+  }
+};
+
