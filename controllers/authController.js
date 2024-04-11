0c727aee320f6cc798a372fb9afab5a5acac6847
Santan21 <lesliegyamfi02@gmail.com>
2024-04-11 15:02:37 +0000

    finished signing up users

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,11 +10,14 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({id: newUser._id,  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET,  {
+    expiresIn: process.env.JWT_EXPIRES_IN 
+  });
 
   res.status(201).json({
     status: 'success',
     data: {
+      token,
         user: newUser
     }, 
   });
