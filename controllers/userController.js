6ce0e893469e91b7e2433fe782191c6561767782
Santan21 <lesliegyamfi02@gmail.com>
2024-04-13 13:06:47 +0000

    logging in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,24 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet',
-  });
-};
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        results: users.length,
+        users,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
