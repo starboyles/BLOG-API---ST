7d4096c0040437aa0ccd72599433e1737caef2ec
Kwame Sarfo <lesliegyamfi02@gmail.com>
2024-06-16 10:35:28 +0000

    add: modify controller to parse & use query param

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -29,7 +29,25 @@ exports.checkID = (req, res, next) => {
 
 exports.getAllBlogs = catchAsync(async (req, res, next) => {
   try {
-    const blogs = await Blog.find();
+    // Build the query object based on the request query parameters
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    // Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    // Execute the query
+    let query = Blog.find(JSON.parse(queryStr));
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    const blogs = await query;
+
     res.status(200).json({
       status: 'Success',
       data: {
